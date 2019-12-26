7d63c35fb4851b36c44051c06ab18d80dc635790
grupo2bioinfor <58787410+grupo2bioinfor@users.noreply.github.com>
2019-12-26 19:04:13 +0000

    Update untitled1.py

--- untitled1.py
+++ untitled1.py
@@ -12,39 +12,39 @@ Created on Wed Dec  4 15:05:12 2019
 import csv
 from Bio import SeqIO
 from Bio import Entrez
-Entrez.email = "A.N.Other@example.com"  # Always tell NCBI who you are
-#handle = Entrez.esearch(db='pubmed', term='EIF2AK4') #a pubmed é uma base de dados de art ncbi
-#record = Entrez.read(handle) #o read da acesso aos artigos de forma ordenada
-#handle.close()
-#print(record)
+Entrez.email = "A.N.Other@example.com" 
+handle = Entrez.esearch(db='pubmed', term='EIF2AK4')
+record = Entrez.read(handle) #o read da acesso aos artigos de forma ordenada
+handle.close()
+print(record)
 #efetch faz download de um record
 #esearch faz uma pesquisa
 ##aceder ao ncbi
 #
 
-#nucleotide_ids = ["NM_001013703.4"]
-#l=[]
-#for i in nucleotide_ids:
-#    handle = Entrez.efetch(db="nucleotide", id=i, rettype="gb", retmode="text")
-#    record = SeqIO.read(handle, "genbank")
-#    handle.close()
+nucleotide_ids = ["NM_001013703.4"]
+l=[]
+for i in nucleotide_ids:
+    handle = Entrez.efetch(db="nucleotide", id=i, rettype="gb", retmode="text")
+    record = SeqIO.read(handle, "genbank")
+    handle.close()
 #    
-#    d={}
-#    for feat in record.features:
-#       if feat.type=='CDS':    #CDS para obter a coding sequence
-#           key_list=['product', 'gene', 'note', 'codon_start','protein_id','translation']
-#           for key in key_list:
-#               d[key]= ''.join(feat.qualifiers[key])
-#    keys_annot=['molecule_type','topology','organism','taxonomy','comment']
-#    for key in keys_annot:
-#        if key=='comment':
-#            d[key]=record.annotations[key].replace('\n', ' ')
-#        else:
-#            d[key]=record.annotations[key]
-#    
-#    l.append(d)
-#print(l)
-##
+    d={}
+    for feat in record.features:
+       if feat.type=='CDS':    #CDS para obter a coding sequence
+           key_list=['product', 'gene', 'note', 'codon_start','protein_id','translation']
+           for key in key_list:
+               d[key]= ''.join(feat.qualifiers[key])
+    keys_annot=['molecule_type','topology','organism','taxonomy','comment']
+    for key in keys_annot:
+        if key=='comment':
+            d[key]=record.annotations[key].replace('\n', ' ')
+        else:
+            d[key]=record.annotations[key]
+    
+    l.append(d)
+print(l)
+
 #
 ## do gene
 #
@@ -64,7 +64,3 @@ for d in l:
         file.write(str(value) + "\t")
     file.write('\n')
 file.close()
-###    
-#
-#
-#
