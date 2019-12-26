587b1a6277f2052875bd8c74d76dd5e330006acd
grupo2bioinfor <58787410+grupo2bioinfor@users.noreply.github.com>
2019-12-26 19:06:14 +0000

    Update and rename untitled2.py to eif2ak4_blast.py

--- eif2ak4_blast.py
+++ eif2ak4_blast.py
@@ -13,47 +13,47 @@ from Bio import SearchIO
 from Bio import SeqIO
 
 
-#file1=open('gene2.xls')
-#lines=file1.readlines()
+file1=open('gene2.xls')
+lines=file1.readlines()
 #
-#file1.close()
+file1.close()
 ##
 #
-#for i in range(len(lines)):
-#    lines[i]=lines[i].split('\t')
+for i in range(len(lines)):
+    lines[i]=lines[i].split('\t')
 #
-#isoform='eIF-2-alpha kinase GCN2'
+isoform='eIF-2-alpha kinase GCN2'
 #
-#for i in range(len(lines)):
-#    if lines[i][0]==isoform:
-#        sequence=lines[i][5]
+for i in range(len(lines)):
+    if lines[i][0]==isoform:
+        sequence=lines[i][5]
 #
 #
 ##        
 ##        
-#fich=open('fstform2.fasta', 'w')
-#fich.write('>'+isoform+'\n'+sequence)
+fich=open('fstform2.fasta', 'w')
+fich.write('>'+isoform+'\n'+sequence)
 #
-#fich.close()
+fich.close()
 ##
 #
-#record = SeqIO.read("fstform2.fasta", format="fasta")
+record = SeqIO.read("fstform2.fasta", format="fasta")
 
 
 ##SeqIO lê em formato fasta
 ##
-#result_handle = NCBIWWW.qblast("blastp", "swissprot", record) #o ultimo parametro, pondo a seq, tinha de estar em fasta para ser lido
+result_handle = NCBIWWW.qblast("blastp", "swissprot", record) #o ultimo parametro, pondo a seq, tinha de estar em fasta para ser lido
 ##blastp para proteinas
 ##blastn para nucleotidos
 ##utilizou-se para proteinas porque caso se tenha verificado a troca de um nucleotido, o aminoacido pode ser o mm-redundancia dos aminoacidos
 ##utilizou-se a swissprot porque é uma fonte mais fidedigna
 ##
-#file= open("my_blast2.xml", "w")
+file= open("my_blast2.xml", "w")
 ##
-#file.write(result_handle.read())
+file.write(result_handle.read())
 ##    
-#file.close()
-#result_handle.close()
+file.close()
+result_handle.close()
 
 
 #Foi necessario criar um ficheiro com o blast para que este pudesse ser acedido mais que uma vez
@@ -64,12 +64,12 @@ blast_record = NCBIXML.parse(result_handle)
 
 #
 arq=open('best_score2.xml', 'w')
-e_value = 0.05
+e_value = 10**(-35)
 
 for rec in blast_record:
     for alignment in rec.alignments:
         for hsp in alignment.hsps:
-            if hsp.expect < e_value:
+            if hsp.expect < e_value and hsp.score > 130:
                 arq.write('****Alignment****'+'\n'+'sequence:'+alignment.title+'\n'+
                           'lenghth:'+ str(alignment.length)+'\n'+'e-value:'+ str(hsp.expect)+'\n'+
                           hsp.query +'\n'+hsp.match +'\n'+
