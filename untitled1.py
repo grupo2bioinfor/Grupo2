484a841fd574a4aa95d7fb0b15ab3c3c724f5c54
grupo2bioinfor <58787410+grupo2bioinfor@users.noreply.github.com>
2019-12-26 18:53:24 +0000

    Add files via upload

---
+++ untitled1.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  4 15:05:12 2019
+
+@author: Angelica
+"""
+
+#; Análise de literatura; análise da sequência e das features presentes no NCBI  
+
+
+#
+import csv
+from Bio import SeqIO
+from Bio import Entrez
+Entrez.email = "A.N.Other@example.com"  # Always tell NCBI who you are
+#handle = Entrez.esearch(db='pubmed', term='EIF2AK4') #a pubmed é uma base de dados de art ncbi
+#record = Entrez.read(handle) #o read da acesso aos artigos de forma ordenada
+#handle.close()
+#print(record)
+#efetch faz download de um record
+#esearch faz uma pesquisa
+##aceder ao ncbi
+#
+
+#nucleotide_ids = ["NM_001013703.4"]
+#l=[]
+#for i in nucleotide_ids:
+#    handle = Entrez.efetch(db="nucleotide", id=i, rettype="gb", retmode="text")
+#    record = SeqIO.read(handle, "genbank")
+#    handle.close()
+#    
+#    d={}
+#    for feat in record.features:
+#       if feat.type=='CDS':    #CDS para obter a coding sequence
+#           key_list=['product', 'gene', 'note', 'codon_start','protein_id','translation']
+#           for key in key_list:
+#               d[key]= ''.join(feat.qualifiers[key])
+#    keys_annot=['molecule_type','topology','organism','taxonomy','comment']
+#    for key in keys_annot:
+#        if key=='comment':
+#            d[key]=record.annotations[key].replace('\n', ' ')
+#        else:
+#            d[key]=record.annotations[key]
+#    
+#    l.append(d)
+#print(l)
+##
+#
+## do gene
+#
+##
+##
+#
+#    
+file=open('gene2.xls', 'w')
+header=['isoform', 'gene', 'note', 'codon_start','protein_id','translation','molecule_type',
+        'topology','organism','taxonomy','comment']
+for col in header:
+    file.write(col+'\t')
+file.write('\n')
+    
+for d in l:
+    for value in d.values():
+        file.write(str(value) + "\t")
+    file.write('\n')
+file.close()
+###    
+#
+#
+#
